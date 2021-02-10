d1bbe43ae54f3d46dd2c8107539aa77b0a2288d9
Brenner Fagundes <brenner.araujo@grupolpj.com>
2021-02-10 12:03:03 -0300

    endpoints basicos de produtos e crud completo

---
+++ app/Models/Product.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Models;
+
+use Eloquent as Model;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+/**
+ * @SWG\Definition(
+ *      definition="Product",
+ *      required={""},
+ *      @SWG\Property(
+ *          property="id",
+ *          description="id",
+ *          type="integer",
+ *          format="int32"
+ *      ),
+ *      @SWG\Property(
+ *          property="name",
+ *          description="name",
+ *          type="string"
+ *      ),
+ *      @SWG\Property(
+ *          property="description",
+ *          description="description",
+ *          type="string"
+ *      ),
+ *      @SWG\Property(
+ *          property="quantity",
+ *          description="quantity",
+ *          type="integer",
+ *          format="int32"
+ *      ),
+ *      @SWG\Property(
+ *          property="created_at",
+ *          description="created_at",
+ *          type="string",
+ *          format="date-time"
+ *      ),
+ *      @SWG\Property(
+ *          property="updated_at",
+ *          description="updated_at",
+ *          type="string",
+ *          format="date-time"
+ *      )
+ * )
+ */
+class Product extends Model
+{
+
+    use HasFactory;
+
+    public $table = 'product';
+    
+    const CREATED_AT = 'created_at';
+    const UPDATED_AT = 'updated_at';
+
+
+
+
+    public $fillable = [
+        'name',
+        'description',
+        'quantity'
+    ];
+
+    /**
+     * The attributes that should be casted to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'id' => 'integer',
+        'name' => 'string',
+        'description' => 'string',
+        'quantity' => 'integer'
+    ];
+
+    /**
+     * Validation rules
+     *
+     * @var array
+     */
+    public static $rules = [
+        'name' => 'nullable|string|max:100',
+        'description' => 'nullable|string',
+        'quantity' => 'nullable|integer',
+        'created_at' => 'nullable',
+        'updated_at' => 'nullable'
+    ];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     **/
+    public function logProducts()
+    {
+        return $this->hasMany(\App\Models\LogProduct::class, 'id_product');
+    }
+}
---
+++ app/Repositories/ProductRepository.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Product;
+use App\Repositories\BaseRepository;
+
+/**
+ * Class ProductRepository
+ * @package App\Repositories
+ * @version February 10, 2021, 2:58 pm UTC
+*/
+
+class ProductRepository extends BaseRepository
+{
+    /**
+     * @var array
+     */
+    protected $fieldSearchable = [
+        'name',
+        'description',
+        'quantity'
+    ];
+
+    /**
+     * Return searchable fields
+     *
+     * @return array
+     */
+    public function getFieldsSearchable()
+    {
+        return $this->fieldSearchable;
+    }
+
+    /**
+     * Configure the Model
+     **/
+    public function model()
+    {
+        return Product::class;
+    }
+}
